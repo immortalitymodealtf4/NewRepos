664534da931e15296740c16cfc5519168fc4b4b3
immortalitymodealtf4 <maxmartynyak@gmail.com>
2019-10-10 21:12:24 +0300

    str revers iterator

--- Iterator.py
+++ Iterator.py
@@ -1,6 +1,6 @@
 class Iterator:
-
-    def __init__(self, some_iterables = 0):
+    """ Numeric iterator"""
+    def __init__(self, some_iterables=0):
         self.some_iterables = some_iterables
 
     def __iter__(self):
@@ -8,20 +8,56 @@ class Iterator:
         return self
 
     def __next__(self):
-        if self.length <= self.some_iterables:
+        if self.length < self.some_iterables:
             res = self.length
             self.length += 1
-            print(res)
             return res
         else:
             raise StopIteration
 
 
-new_iter = Iterator(4)
-i = iter(new_iter)
-next(i)
-next(i)
-next(i)
-next(i)
-next(i)
-next(i)
+class StrIterator:
+    """ Iterator for string """
+    def __init__(self, text=""):
+        self.text = text
+        self.str_length = len(self.text)
+        self.char = self.text[self.str_length - 1]
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.str_length > 0:
+            res = self.char
+            self.str_length -= 1
+            self.char = self.text[self.str_length-1]
+        else:
+            raise StopIteration
+        return res, self.str_length
+
+
+str_iterator = StrIterator("Hello world")
+str_iter = iter(str_iterator)
+print(next(str_iter))
+print(next(str_iter))
+print(next(str_iter))
+print(next(str_iter))
+print(next(str_iter))
+print(next(str_iter))
+print(next(str_iter))
+print(next(str_iter))
+print(next(str_iter))
+print(next(str_iter))
+print(next(str_iter))
+
+num_iter = Iterator(8)
+num_iterable = iter(num_iter)
+print(next(num_iterable))
+print(next(num_iterable))
+print(next(num_iterable))
+print(next(num_iterable))
+print(next(num_iterable))
+print(next(num_iterable))
+print(next(num_iterable))
+print(next(num_iterable))
+print(next(num_iterable))
